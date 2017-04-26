64387a36c91e17ab5e3b9b8b64e9c18e4064252b
Nigel Lee <lihui8@le.com>
2017-04-26 20:27:12 +0800

    方法总结

--- Algor/Algor/ViewController.m
+++ Algor/Algor/ViewController.m
@@ -20,13 +20,25 @@
     
     // reverse a string 逆序字符串
     NSString *base = @"abcdefg";
+    // 方法-1
     NSMutableString *str = [NSMutableString stringWithCapacity:base.length];
     for (NSInteger i = base.length - 1; i>= 0; --i) {
         unichar ch = [base characterAtIndex:i];
         [str appendFormat: @"%c", ch];
     }
     
+    // 方法-2
+    NSMutableArray *temp=[[NSMutableArray alloc] init];
+    for(int i=0;i<[base length];i++) {
+        [temp addObject:[NSString stringWithFormat:@"%c",[base characterAtIndex:i]]];
+    }
+    temp = [NSMutableArray arrayWithArray:[[temp reverseObjectEnumerator] allObjects]];
+    NSString *reverseString=@"";
+    for(int i=0;i<[temp count];i++) {
+        reverseString=[NSString stringWithFormat:@"%@%@",reverseString,[temp objectAtIndex:i]];
+    }
     
+    [self.navigationController popToViewController:self animated:NO];
     
 }
 
